a1d83765bd4e153c4d82bd4c482a08f4dd2e7291
Hanna Fjeldsaa <fjeldsaa@hotmail.com>
2023-10-06 21:25:31 +0200

    Checkout success page done

--- src/Components/Component/Cart.js
+++ src/Components/Component/Cart.js
@@ -1,23 +1,18 @@
 import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import Overlay from "react-bootstrap/Overlay";
 import { FaShoppingCart } from "react-icons/fa";
 import "./Cart.css";
 import Badge from 'react-bootstrap/Badge';
 
 export function SeeCart(props) {
 	const { onRemove, onAdd, cartItems } = props 
-	const [show, setShow] = useState(false);
-	const target = useRef(null);
 	const countCartItems = cartItems.length;
 	const productNumbers = localStorage.getItem("TotalCart");
-	console.log(cartItems);
-	console.log(countCartItems)
 
 	return (
 		<div className="FixedCart mt-5">
 			<div className="CartBlue bg-info rounded-start-pill">
-			<Link ref={target} onClick={() => setShow(!show)}>
+			<Link to="/checkout">
 				<FaShoppingCart id="shopping-cart" className="fs-5 text-light" />
 			</Link>
 			<Badge bg="dark" style={{
--- src/Components/Layouts/CheckoutSuccessPage.js
+++ src/Components/Layouts/CheckoutSuccessPage.js
@@ -1,8 +1,18 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Alert from 'react-bootstrap/Alert';
 
 function CheckoutSuccessLayout() {
-	return <div>Checkout Success Page</div>;
+	return (
+		<div className="">
+			<div className=" m-auto mt-5 text-center">
+				<img src="SuccessImg.jpg" alt="Purchase was successful." />
+			</div>
+			<Alert variant="info" className="mt-4 mx-auto text-center" style={{maxWidth: "400px"}}>
+				Your order is now placed in our systems!<br/>
+				<Alert.Link href="/"> Click here if you want to get back to store</Alert.Link>.
+        	</Alert>
+		</div>
+	);
 }
 
 export default CheckoutSuccessLayout;
